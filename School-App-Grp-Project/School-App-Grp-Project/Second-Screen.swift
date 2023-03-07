4831b296aa1a5d5d9d5cc1e047d5bdf054f9f48b
NATEKAR, ANANT <anantnatekar@northwesternmutual.com>
2023-03-07 00:22:41 -0600

    Committ

---
+++ School-App-Grp-Project/School-App-Grp-Project/Second-Screen.swift
@@ -0,0 +1,20 @@
+//
+//  Second-Screen.swift
+//  School-App-Grp-Project
+//
+//  Created by Anant Natekar on 2/26/23.
+//
+
+import SwiftUI
+
+struct Second_Screen: View {
+    var body: some View {
+        Text("You are in Second Screen!")
+    }
+}
+
+struct Second_Screen_Previews: PreviewProvider {
+    static var previews: some View {
+        Second_Screen()
+    }
+}
